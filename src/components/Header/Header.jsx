e01ea7f26451222ff79de9abdba87385970e9186
MeralSavas <meralsavas@gmail.com>
2022-10-11 18:54:54 +0100

    created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Popular Tour PLaces</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <a href="#aboutus">About Us</a>
+      <a href="#foryou">For You</a>
+      <a href="#services">Services</a>
+      <a href="#blog">Blog</a>
+      <a href="#vlog">Vlog</a>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
+
+export default NavBar;
