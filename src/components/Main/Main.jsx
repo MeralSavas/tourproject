819b02ef45fa7bac930ea9b8d01f85e89cc15ea1
MeralSavas <meralsavas@gmail.com>
2022-10-11 20:04:05 +0100

    added pictures

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,22 @@
 import "./Main.scss";
+import { data } from "../../helper/data";
+import Card from "./Card";
 
 const Main = () => {
+  console.log(data);
   return (
     <div className="card-container">
-      <h1>CARD</h1>
+      {data.map(
+        (item, index) => (
+          <Card {...item} key={index} />
+        )
+        // return (
+        //   <div className="cards">
+        //     <h1>{item.title}</h1>
+        //     <img src={item.image} alt="img" />
+        //   </div>
+        // );
+      )}
     </div>
   );
 };
